f542303156b2218d1cd9d48ab71975ecced494e4
thiagojlfm <thiagomarcionilio@gmail.com>
2026-07-16 21:25:41 -0300

    Fix: renomear evento ready para clientReady + remover log de diagnostico do DB

--- src/events/ready.js
+++ src/events/ready.js
@@ -26,17 +26,10 @@ async function registrarComandos() {
 }
 
 module.exports = {
-  name: 'ready',
+  name: 'clientReady',
   once: true,
   async execute(client) {
     console.log(`[ready] Logado como ${client.user.tag}`);
-
-    // Diagnóstico de persistência do volume
-    const { dbPath } = require('../config/config');
-    const veiculosFile = path.join(dbPath, 'veiculos.json');
-    const existe = fs.existsSync(veiculosFile);
-    const tamanho = existe ? fs.statSync(veiculosFile).size : 0;
-    console.log(`[db] path=${dbPath} veiculos.json existe=${existe} tamanho=${tamanho}b`);
 
     await registrarComandos();
 
